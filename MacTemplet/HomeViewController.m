96c9f01c46ed2fc9c8f50c9419bcf7d71b351659
Shang <shang1219178163@gmail.com>
2019-06-10 16:26:08 +0800

    update

--- MacTemplet/HomeViewController.m
+++ MacTemplet/HomeViewController.m
@@ -19,6 +19,8 @@
 @property (nonatomic, strong) NNView *bottomView;
 @property (nonatomic, strong) NSTextField *textField;
 @property (nonatomic, strong) NSTextField *textFieldTwo;
+
+@property (nonatomic, strong) NSPopUpButton *popBtn;
 @property (nonatomic, strong) NSButton *btn;
 
 @property (nonatomic, strong) NSArray *list;
@@ -41,12 +43,13 @@
     [self.bottomView addSubview:self.textField];
     [self.bottomView addSubview:self.textViewTwo];
     [self.bottomView addSubview:self.btn];
+    [self.bottomView addSubview:self.popBtn];
     [self.view addSubview:self.bottomView];
 
 
     self.list = @[self.scrollView, self.scrollViewTwo];
     
-    
+
     self.textView.string = @"---有问题的NSTextView---\n\n第一杯酒，阳光明媚，窗外的青藤爬进了我的眼。\n第二杯酒，春风轻漾，叶梢轻拂着我的眉。\n第三杯酒，鸟儿鸣叫，轻啄着我的心。\n第四杯酒，影上窗楣，让我忘了我是谁。\n第五杯酒，少年将飞，穿越层林叠翠。\n第六杯酒，十六轻狂，笑斥天下执念。\n第七杯酒，人上心头，紫衣白裙小马尾。\n第八杯酒，月光轻舞，你的脸儿红云飘。\n第九杯酒，大漠孤烟，未知的前程孤独的脚印。\n第十杯酒，长河旭日，谁家孩儿无齿地笑。\n十一杯酒，群山苍翠，有个老翁枕石而醉。\n十二杯酒，临渊而窥，山崖还给年岁。\n十三杯酒，蜗牛有角，彼世界如此世界一般疲惫。\n十四杯酒，迷眼渐累，火堆旁的人们渐要沉睡。\n十五杯酒，形只影单，远方的人儿可曾安睡。\n十六杯酒，抬头望月，白衣白裙与白兔。\n十七杯酒，漫天星星，谁真谁幻谁在乎。\n十八杯酒，残酒映月，谁的容颜依稀浮现。\n十九杯酒，仰天长啸，该死的老天操蛋的命运。\n二十杯酒，闭了双眼，是是非非皆已不见。\n二十一杯酒，想起妹妹，你的虎牙为谁而笑。\n二十二杯酒，我的弟弟，是否忙着拯救地球。\n二十三杯酒，想起妈妈，你的头发烤面包啦。\n二十四杯酒，我的朋友，天堂的你可曾安好。\n二十五杯酒，想起父亲，窗外的雨点坠了下来。\n二十六杯酒，乌蝇不飞，若心悸的你我躲在叶下看秋雨渐衰。\n二十七杯酒，弹几点泪，轻轻放下酒杯。\n......";
     
     
@@ -95,6 +98,12 @@
         make.bottom.equalTo(self.bottomView.superview).offset(-padding);
     }];
     
+    [self.popBtn makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.textField.superview).offset(padding);
+        make.right.equalTo(self.btn.left).offset(-padding);
+        make.width.equalTo(120);
+        make.bottom.equalTo(self.bottomView.superview).offset(-padding);
+    }];
 }
 
 #pragma mark -funtions
@@ -149,7 +158,6 @@
             NSTextView * view = [[NSTextView alloc]init];
             view.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
 
-            view.backgroundColor = NSColor.greenColor;
             view;
         });
     }
@@ -164,7 +172,7 @@
 
 //            view.hasHorizontalScroller = true;
             view.hasVerticalScroller = true;
-            view.backgroundColor = NSColor.randomColor;
+            view.backgroundColor = NSColor.yellowColor;
             view;
         });
     }
@@ -179,7 +187,7 @@
 
             view.hasHorizontalScroller = true;
             view.hasVerticalScroller = true;
-            view.backgroundColor = NSColor.randomColor;
+            view.backgroundColor = NSColor.greenColor;
             view;
         });
     }
@@ -240,6 +248,25 @@
     return _textFieldTwo;
 }
 
+- (NSPopUpButton *)popBtn{
+    if (!_popBtn) {
+        _popBtn = ({
+            NSPopUpButton *view = [[NSPopUpButton alloc] initWithFrame:CGRectMake(0, 100, 100, 30) pullsDown:false];
+            view.autoenablesItems = true;
+            
+            NSArray *list = @[@"北京", @"上海", @"广州", @"深圳"];
+            [view addItemsWithTitles:list];
+            [view addActionHandler:^(NSControl * _Nonnull control) {
+                NSPopUpButton * sender = control;
+                NSLog(@"%@", sender.titleOfSelectedItem);
+
+            } forControlEvents:NSEventMaskLeftMouseDown];
+            
+            view;
+        });
+    }
+    return _popBtn;
+}
 
 -(NSButton *)btn{
     if (!_btn) {
@@ -248,7 +275,7 @@
             view.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
 
             view.title = @"保存";
-            [view setContentTintColor:NSColor.blueColor];
+            [view setContentTintColor:NSColor.redColor];
             [view addActionHandler:^(NSControl * _Nonnull control) {
                 NSLog(@"%@", control);
                 
