dd1fb65a4e164524d0b3beede8084ec0c959fbe3
Shang <shang1219178163@gmail.com>
2019-11-24 10:34:31 +0800

    模糊视图

--- MacTemplet/NSVisualEffectController.swift
+++ MacTemplet/NSVisualEffectController.swift
@@ -8,11 +8,25 @@
 
 import Cocoa
 
+@available(OSX 10.14, *)
 class NSVisualEffectController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        view.layer?.backgroundColor = NSColor.lightBlue.cgColor;
+        
+        let imgViewRect = CGRectMake(10, 10, view.bounds.size.width, view.bounds.size.height*0.5)
+        let imgView = NSImageView(frame: imgViewRect)
+        imgView.image = NSApplication.appIcon
+        view.addSubview(imgView)
+        
+        let rect = CGRectMake(10, 10, view.bounds.size.width*0.5, view.bounds.size.height*0.5)
+        view.addVisualEffectView(rect)
+        
+        
+
     }
     
 }
