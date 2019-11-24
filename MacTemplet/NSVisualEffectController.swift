7d1665c78cc50dd340438d1748b37ccdfa4d1120
Shang <shang1219178163@gmail.com>
2019-11-24 10:33:24 +0800

    模糊效果

---
+++ MacTemplet/NSVisualEffectController.swift
@@ -0,0 +1,18 @@
+//
+//  NSVisualEffectController.swift
+//  MacTemplet
+//
+//  Created by Bin Shang on 2019/11/24.
+//  Copyright © 2019 Bin Shang. All rights reserved.
+//
+
+import Cocoa
+
+class NSVisualEffectController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
