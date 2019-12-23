0b19a0705fd002476cf8f6c0f9a6d9779889fdc8
Shang <shang1219178163@gmail.com>
2019-12-23 16:38:31 +0800

    优化默认值

--- MacTemplet/ProppertyLazyController.m
+++ MacTemplet/ProppertyLazyController.m
@@ -164,6 +164,7 @@
             NNLabel * view = [[NNLabel alloc]initWithFrame:CGRectZero];
             view.bordered = false;  ///是否显示边框
             view.font = [NSFont systemFontOfSize:13];
+            view.font = [NSFont fontWithName:NSFont.kPingFangThin size:13];
             view.textColor = NSColor.grayColor;
 //            view.textColor = NSColor.lightBlue;
             view.alignment = NSTextAlignmentCenter;
