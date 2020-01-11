0c77521babe1c8c7fbabaac754f32c9bd1396446
Shang <shang1219178163@gmail.com>
2020-01-11 15:30:15 +0800

    update

---
+++ MacTemplet/JsonToModel/NNViewModel.swift
@@ -0,0 +1,98 @@
+//
+//  NNViewModel.swift
+//  MacTemplet
+//
+//  Created by Bin Shang on 2020/1/11.
+//  Copyright © 2020 Bin Shang. All rights reserved.
+//
+
+import Cocoa
+
+class NNViewModel: NSObject {
+
+    /// 获取类内容
+    static func getContent(with name: String, type: String) -> String {
+        let copyRight = NNCopyRightModel.getCopyRight(with: "\(name)", type: type)
+        return """
+\(copyRight)
+
+import UIKit
+
+import SnapKit
+import SwiftExpand
+        
+///
+class \(name): UIView {
+
+    
+    typealias ViewClick = (\(name), Int) -> Void;
+    var viewBlock: ViewClick?;
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    
+        
+    }
+    
+    // MARK: - funtions
+    func block(_ action:@escaping ViewClick) {
+        self.viewBlock = action;
+    }
+    
+        
+    //MARK: - lazy
+    lazy var btnCancell:UIButton = {
+        let view = UIButton(type: .custom);
+        view.tag = 0;
+        
+        view.titleLabel?.font = UIFont.systemFont(ofSize: 16);
+        view.setTitle(kTitleCancell, for: .normal);
+        view.setTitleColor(UIColor.red, for: .normal);
+        view.addActionHandler({ (control) in
+            if let sender = control as? UIButton {
+                self.viewBlock!(self,sender.tag);
+                self.dismiss();
+            }
+
+        }, for: .touchUpInside)
+        return view;
+    }();
+    
+    lazy var label:UILabel = {
+        let view = UILabel(frame: .zero);
+        view.text = "请选择";
+        view.textColor = UIColor.gray;
+        view.textAlignment = .center;
+        return view;
+    }();
+    
+    lazy var btnSure:UIButton = {
+        let view = UIButton(type: .custom);
+        view.tag = 1;
+        
+        view.titleLabel?.font = UIFont.systemFont(ofSize: 16);
+        view.setTitle(kTitleSure, for: .normal);
+        view.setTitleColor(UIColor.theme, for: .normal);
+        view.addActionHandler({ (control) in
+            if let sender = control as? UIButton {
+                self.viewBlock!(self,sender.tag);
+            }
+
+        }, for: .touchUpInside)
+        return view;
+    }();
+}
+
+"""
+    }
+}
