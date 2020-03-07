d1a96c0e3c717c4e3a307a22c41ab853b62ea6ae
Shang <shang1219178163@gmail.com>
2020-03-07 16:30:54 +0800

    添加修改API

---
+++ MacTemplet/JsonToModel/NNRequestAPIUpdateCreater.swift
@@ -0,0 +1,13 @@
+//
+//  NNRequestAPIUpdateCreater.swift
+//  MacTemplet
+//
+//  Created by Bin Shang on 2020/3/7.
+//  Copyright © 2020 Bin Shang. All rights reserved.
+//
+
+import Cocoa
+
+class NNRequestAPIUpdateCreater: NSObject {
+
+}
