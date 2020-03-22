9f0c7dcf8beb6ed8d934d6f711c7b48227c47711
Shang <shang1219178163@gmail.com>
2020-03-22 11:42:28 +0800

    自定义NSButton

--- MacTemplet/NNBaseView/AAButton.h
+++ MacTemplet/NNBaseView/AAButton.h
@@ -12,6 +12,31 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface AAButton : NSButton
 
+/**
+ 背景色 - 默认是APP的蓝色按钮
+ */
+@property (strong) NSColor *backgroundColor;
+
+/**
+ 阴影偏移量 - 如果不需要阴影请不要设置
+ */
+@property (assign) CGSize shadowOffset;
+
+/**
+ 圆角的半径 - 默认为4
+ */
+@property (assign) CGFloat cornerRadius;
+
+
+/**
+ 设置标题 - 这只是一个快捷方法，你可以根据富文本内容设置更多自定标题
+
+ @param title 内容
+ @param textColor 颜色
+ */
+- (void)setTitle:(NSString *)title color:(NSColor *)textColor font:(CGFloat)fontsize;
+
+
 @end
 
 NS_ASSUME_NONNULL_END
--- MacTemplet/NNBaseView/AAButton.m
+++ MacTemplet/NNBaseView/AAButton.m
@@ -10,10 +10,92 @@
 
 @implementation AAButton
 
+- (instancetype)initWithFrame:(NSRect)frameRect {
+    if (self = [super initWithFrame:frameRect]) {
+        [self setup];
+    }
+    return self;
+}
+
+- (instancetype)init {
+    if (self = [super init]) {
+        [self setup];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder {
+    if (self = [super initWithCoder:coder]) {
+        [self setup];
+    }
+    return self;
+}
+
+- (void)setup {
+    _backgroundColor = [NSColor whiteColor];
+    _shadowOffset = CGSizeZero;
+    _cornerRadius = 4.f;
+}
+
+- (void)setTitle:(NSString *)title color:(NSColor *)textColor font:(CGFloat)fontsize{
+    
+    NSMutableParagraphStyle *paraStyle = [[NSMutableParagraphStyle alloc] init];
+    paraStyle.alignment = NSTextAlignmentCenter;
+    
+    NSDictionary *attDic = @{
+        NSParagraphStyleAttributeName: paraStyle,
+        NSForegroundColorAttributeName: textColor,
+        NSFontAttributeName:  [NSFont systemFontOfSize:fontsize],
+            
+    };
+    NSMutableAttributedString *attString = [[NSMutableAttributedString alloc] initWithString:title attributes:attDic];
+    self.attributedTitle = attString;
+}
+
+//- (void)setTitle:(NSString *)title color:(NSColor *)textColor font:(CGFloat)fontsize{
+//    NSMutableAttributedString *attrTitle = [[NSMutableAttributedString alloc] initWithString:title];
+//    NSUInteger len = [attrTitle length];
+//    NSRange range = NSMakeRange(0, len);
+//    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+//    paragraphStyle.alignment = NSTextAlignmentCenter;
+//    [attrTitle addAttribute:NSForegroundColorAttributeName value:textColor
+//                      range:range];
+//    [attrTitle addAttribute:NSFontAttributeName value:[NSFont systemFontOfSize:fontsize]
+//                      range:range];
+//
+//    [attrTitle addAttribute:NSParagraphStyleAttributeName value:paragraphStyle
+//                      range:range];
+//    [attrTitle fixAttributesInRange:range];
+//    [self setAttributedTitle:attrTitle];
+//    attrTitle = nil;
+//    [self setNeedsDisplay:YES];
+//}
+
+/**
+ 绘制方法由updateLayer替换
+ */
 - (void)drawRect:(NSRect)dirtyRect {
     [super drawRect:dirtyRect];
     
-    // Drawing code here.
+}
+
+- (BOOL)wantsUpdateLayer {
+    return YES;
+}
+
+- (void)updateLayer {
+    //changed to the width or height of a single source pixel centered at the specified location.
+    self.layer.contentsCenter = CGRectMake(0.5, 0.5, 0, 0);
+    //setImage
+    self.layer.backgroundColor = _backgroundColor.CGColor;
+    self.layer.cornerRadius = _cornerRadius;
+    if (!CGSizeEqualToSize(CGSizeZero, _shadowOffset)) {
+        self.layer.masksToBounds = NO;
+        self.layer.shadowColor = _backgroundColor.CGColor;
+        self.layer.shadowOffset = CGSizeMake(1, 2);
+        self.layer.shadowRadius = 5;
+        self.layer.shadowOpacity = 1;
+    }
 }
 
 @end
