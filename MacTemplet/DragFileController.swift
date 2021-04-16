46b67a3bd954c25dd84f7c1397c3489be286a0bf
Shang <shang1219178163@gmail.com>
2021-04-16 23:55:41 +0800

    实现文件拖拽功能

--- MacTemplet/DragFileController.swift
+++ MacTemplet/DragFileController.swift
@@ -7,12 +7,151 @@
 //
 
 import Cocoa
+import SnapKit
+import CocoaExpand
 
+@available(macOS 10.13, *)
 class DragFileController: NSViewController {
+    
+    lazy var destinationImageView: NSImageView = {
+        let view = NSImageView()
+        view.imageScaling = .scaleProportionallyDown
+        
+        view.layer?.cornerRadius = 8;
+        return view
+    }()
 
+    lazy var destinationView: DragDestinationView = {
+        let view = DragDestinationView(types: [.tiff, .color, .string, .fileURL, .html])
+        view.delegate = self
+        return view
+    }()
+    
+    
+    lazy var textView: NNTextView = {
+        let view = NNTextView.create(.zero)
+        view.font = NSFont.systemFont(ofSize: 17, weight: .semibold)
+        view.isEditable = false
+        view.backgroundColor = NSColor.systemYellow
+        view.alignment = .center
+        view.string = "描述信息"
+        return view;
+    }()
+    
+    
+    var showAlertAgain = true
+    
+
+    // MARK: -lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        view.addSubview(destinationImageView)
+        view.addSubview(destinationView)
+        view.addSubview(textView.enclosingScrollView!)
+        destinationImageView.layer?.backgroundColor = NSColor.white.cgColor
+        textView.backgroundColor = NSColor.clear
+
+//        destinationView.layer?.backgroundColor = NSColor.systemGreen.cgColor
+//        destinationImageView.layer?.wantsLayer = true
+//        destinationView.layer?.wantsLayer = true
+        
+        view.addGestureClick { (gesture) in
+            self.showAlert()
+        }
     }
     
+    override func viewDidLayout() {
+        super.viewDidLayout()
+        
+        destinationView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+
+        textView.enclosingScrollView?.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(10)
+            make.left.equalToSuperview().offset(-10)
+            make.right.equalToSuperview().offset(-10)
+            make.height.equalTo(25)
+        }
+        
+        destinationImageView.snp.makeConstraints { (make) in
+            make.top.equalTo(textView.enclosingScrollView!.snp.bottom).offset(10)
+            make.centerX.equalToSuperview().offset(0)
+            make.width.equalTo(500)
+            make.height.equalTo(500)
+        }
+                
+//        destinationImageView.snp.makeConstraints { (make) in
+//            make.top.equalTo(textView.enclosingScrollView!.snp.bottom).offset(10)
+//            make.centerX.equalToSuperview().offset(10)
+//            make.width.equalToSuperview().multipliedBy(0.7)
+//            make.bottom.equalToSuperview().offset(-10)
+//        }
+        
+    }
+    
+    func showAlert() {
+        if showAlertAgain == false {
+            return
+        }
+            
+        NSAlert()
+            .messageText("提示")
+            .informativeText("非图片文件")
+            .alertStyle(.warning)
+//             .addButtons(["确定", "取消",])
+            .addButtons(["确定",])
+//            .showsHelp(true)
+//            .showsSuppressionButton(true)
+//            .suppressionButtonAction({ (sender) in
+//                DDLog(sender.state.rawValue)
+//                self.showAlertAgain = sender.state.rawValue == 0;
+//            })
+            .beginSheet { (respone) in
+                print("\(#function)\(respone)")
+            }
+    }
+    
+}
+
+
+@available(OSX 10.13, *)
+extension DragFileController: DragDestinationViewDelegate {
+    
+    func processImage(_ image: NSImage, pasteBoard: NSPasteboard) {
+        destinationImageView.image = image
+        textView.string = "\(image.sizePixels.width) * \(image.sizePixels.height)"
+        
+        if let property = pasteBoard.propertyList?.first as? String {
+            let list = property.components(separatedBy: "/")
+            textView.string = "\(list.last ?? "")(\(Int(image.size.width))x\(Int(image.size.height)))"
+        }
+    }
+    
+    func process(_ obj: Any, pasteBoard: NSPasteboard) {
+        switch obj {
+        case let value as String:
+            print(#function, #line, "String", value)
+            textView.string = value
+            
+            showAlert()
+
+        case let value as NSColor:
+            print(#function, #line, "NSColor", value)
+            destinationImageView.layer?.backgroundColor = value.cgColor
+            textView.textColor = value
+            
+        case let value as URL:
+            print(#function, #line, "URL", value.absoluteString.removingPercentEncoding ?? "")
+            textView.string = value.lastPathComponent.removingPercentEncoding ?? ""
+
+            showAlert()
+
+        default:
+            print(#function, #line, obj)
+            break
+        }
+    }
 }
--- MacTemplet/NNView/DragDestinationView.swift
+++ MacTemplet/NNView/DragDestinationView.swift
@@ -7,13 +7,84 @@
 //
 
 import Cocoa
+import CocoaExpand
 
+@objc protocol DragDestinationViewDelegate: NSObjectProtocol {
+    @objc optional func processImage(_ image: NSImage, pasteBoard: NSPasteboard)
+    
+    @objc optional func process(_ obj: Any, pasteBoard: NSPasteboard)
+}
+
+///拖拽目标视图
+@available(macOS 10.13, *)
 class DragDestinationView: NSView {
-
-    override func draw(_ dirtyRect: NSRect) {
-        super.draw(dirtyRect)
-
-        // Drawing code here.
+    
+    var delegate: DragDestinationViewDelegate?
+    ///支持拖入的类型
+    var supportedTypes: [NSPasteboard.PasteboardType] = [.tiff, .color, .string, .fileURL, .html]{
+        willSet{
+            self.registerForDraggedTypes(newValue)
+        }
+    }
+    ///仅支持传入的类型
+    var acceptableUTITypes: [NSPasteboard.ReadingOptionKey : Any] {
+        let types = [NSImage.imageTypes,
+                     NSString.readableTypeIdentifiersForItemProvider,
+                     NSURL.readableTypeIdentifiersForItemProvider].flatMap { $0 }
+        return [NSPasteboard.ReadingOptionKey.urlReadingContentsConformToTypes : types]
     }
     
+    var isReceivingDrag = false {
+        didSet {
+            needsDisplay = true
+        }
+    }
+    
+    // MARK: -lifecycle
+    convenience init(types: [NSPasteboard.PasteboardType]) {
+        self.init()
+        self.registerForDraggedTypes(types)
+    }
+        
+    // MARK: -drag
+    override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+        return .copy
+    }
+    
+    override func draggingExited(_ sender: NSDraggingInfo?) {
+        isReceivingDrag = false
+    }
+    
+    override func prepareForDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        return true
+    }
+    
+//    override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
+//
+//        isReceivingDrag = false
+//        let pasteBoard = sender.draggingPasteboard
+//        guard let image = NSImage(pasteboard: pasteBoard) else {
+//            return false
+//        }
+//        delegate?.processImage?(image, pasteBoard: pasteBoard)
+//        return true
+//    }
+    
+    override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        isReceivingDrag = false
+        let pasteBoard = sender.draggingPasteboard
+        
+        let classArray: [AnyClass] = [NSImage.self, NSColor.self, NSString.self, NSURL.self]
+        return pasteBoard.readObjects(forClasses: classArray, options: acceptableUTITypes) { (obj) in
+            if let value = obj as? NSImage {
+                self.delegate?.processImage?(value, pasteBoard: pasteBoard)
+            } else {
+                self.delegate?.process?(obj, pasteBoard: pasteBoard)
+            }
+        }
+    }
+    
+    override func concludeDragOperation(_ sender: NSDraggingInfo?) {
+
+    }
 }
